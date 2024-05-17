c7773d318bb885edb6334225052eb522bef56bd5
Md Farhan Islam Shuvro <shuvro.cadet@gmail.com>
2024-05-17 16:23:53 +0600

    Facade

--- Structutral/Facade_Pattern/src/client/Demo.java
+++ Structutral/Facade_Pattern/src/client/Demo.java
@@ -5,6 +5,6 @@ public class Demo {
 	  public static void main(String[] args) {
 	        VideoConversionFacade converter = new VideoConversionFacade();
 	        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
-	        // ....
+	        // .....
 	    }
 }
